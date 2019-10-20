61bd0d75ec10334cc8cb136e84bb0a55cac53aa2
ansari-akram <ansariakramobaid@gmail.com>
2019-10-20 13:49:02 +0530

    Update png_to_jpg.py

--- png_to_jpg.py
+++ png_to_jpg.py
@@ -1,19 +1,29 @@
+# importing modeuls required
 import os
 from PIL import Image
+
+# image path to convert
 image_path = 'A:\\Tuition Photos'
+
+# path to save the images
 directory = 'A:\\dataset'
 
+# code for appending the all jpg files in a list
 files = []
 for r, d, f in os.walk(image_path):
     for file in f:
         if '.jpg' in file:
             files.append(os.path.join(file))
 
-os.chdir('A:\\dataset')
-count = 0
+# change your directory to the directory
+os.chdir(directory)
+
+# code for saving jpg in directory
 for file in files:
-    print(file)
+    #print(file)
     img = Image.open(file)
     img = img.convert('RGB')
-    image_name = file+'.jpg'
+    temp = file[:-4]
+    image_name = temp+'.jpg'
     img.save(image_name, quality=95)
+    print('[INFO] converted ' + file + ' to ' + image_name)
